261af70b0f5fe219fbd2f50bdfd986adfdf382aa
dinohamzic <dino@subtlebits.com>
2020-08-07 15:56:01 +0200

    Create homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import PostList, { GET_PRODUCTS_QUERY } from '../components/ProductsList'
+import ProductsGrid, { GET_PRODUCTS_QUERY } from '../containers/products/ProductsGrid'
+import AppHeader, { GET_TAXONOMIES_QUERY } from '../containers/navigation/AppHeader'
 import { initializeApollo } from '../lib/apolloClient'
 
 const IndexPage = () => (
   <main>
-    <PostList />
+    <AppHeader />
+    <ProductsGrid />
   </main>
 )
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
+
+  await apolloClient.query({
+    query: GET_TAXONOMIES_QUERY
+  })
 
   await apolloClient.query({
     query: GET_PRODUCTS_QUERY
