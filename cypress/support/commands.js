b05705a5494fece1791326f6c8bb30a8aecf54c6
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-09-11 10:29:46 +0200

    Allow Cypress to mock GraphQL requests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,26 @@
 // ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const responseStub = (result) => Promise.resolve({
+  json: () => Promise.resolve(result),
+  text: () => Promise.resolve(JSON.stringify(result)),
+  ok: true
+})
+
+Cypress.Commands.add('stubRequest', (fixture, operationName) => {
+  cy.fixture(fixture).then((data) => {
+    cy.on('window:before:load', (win) => {
+      const fetchGraphQl = () => {
+        return responseStub(data[operationName])
+      }
+
+      cy.stub(win, 'fetch')
+        .withArgs(Cypress.env('NEXT_PUBLIC_GRAPHQL_URI'))
+        .as('fetchGraphQl')
+        .callsFake(fetchGraphQl)
+    })
+  })
+})
