662001fd36b81996e43efde17ac4fef2070eb2ca
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-10-09 12:53:39 +0200

    Add global style using styled-component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 
 import 'normalize.css'
 
 import { useApollo } from '../lib/apolloClient'
 import { theme } from '../lib/theme'
+
+const GlobalStyle = createGlobalStyle`
+  a {
+    color: ${(props) => props.theme.colors.copy.light};
+  }
+`
 
 export default function App ({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
@@ -12,6 +18,7 @@ export default function App ({ Component, pageProps }) {
   return (
     <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
