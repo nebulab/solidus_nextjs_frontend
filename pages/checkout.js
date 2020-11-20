2df279ce4a4d140888fcc02ff6cdd1232ad141b3
dinohamzic <dino@subtlebits.com>
2020-11-20 11:38:56 +0100

    temp commit 2

---
+++ pages/checkout.js
@@ -0,0 +1,55 @@
+import { gql, useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
+
+const GET_ORDER_QUERY = gql`
+  query GetCurrentOrder {
+    currentOrder {
+      id
+      number
+      lineItems {
+        nodes {
+          id
+          variant {
+            sku
+          }
+          quantity
+        }
+      }
+    }
+  }
+`
+
+const CheckoutPage = () => {
+  const router = useRouter()
+  const { loading, error, data } = useQuery(GET_ORDER_QUERY)
+
+  if (error) {
+    return <div>Error loading order.</div>
+  }
+
+  if (loading) {
+    return <div>Loading</div>
+  }
+
+  const { lineItems } = data?.currentOrder
+
+  if (lineItems?.nodes.length === 0) {
+    router.push('/')
+    return null
+  }
+
+  return (
+    <main>
+      <h1>Checkout</h1>
+      <ul>
+        {lineItems.nodes.map((lineItem) => (
+          <li key={lineItem.id}>
+            {lineItem.variant.sku} - Quantity: {lineItem.quantity}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default CheckoutPage
